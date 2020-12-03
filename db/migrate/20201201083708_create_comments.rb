2bed0b6e9cdc038cdea3e094899a603952471048
Nexch <mauricio.t.1999@gmail.com>
2020-12-03 01:57:06 -0500

    solve erros in the code

--- db/migrate/20201201083708_create_comments.rb
+++ db/migrate/20201201083708_create_comments.rb
@@ -1,10 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
-    drop_table :comments
     create_table :comments do |t|
-      t.text :body
-      t.integer :user_id
-      t.integer :post_id
+      t.string :user_id
+      t.string :post_id
 
       t.timestamps
     end
